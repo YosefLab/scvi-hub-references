c81927f63888be4bfe22f9b12c2db22ca83e4127
Martin Kim <martinkim@berkeley.edu>
2023-03-13 12:52:10 -0700

    Update lung cancer

--- non_small_cell_lung_cancer/main.py
+++ non_small_cell_lung_cancer/main.py
@@ -1,4 +1,6 @@
+import json
 import os
+import pathlib
 from pathlib import Path
 from typing import Tuple
 
@@ -6,6 +8,12 @@ import anndata as ad
 import pooch
 import scanpy as sc
 import scvi
+
+
+def make_parents(*paths) -> None:
+    """Make parent directories of a file path if they do not exist."""
+    for p in paths:
+        pathlib.Path(p).parent.mkdir(parents=True, exist_ok=True)
 
 
 def load_model_and_dataset() -> Tuple[scvi.model.SCANVI, ad.AnnData]:
@@ -30,17 +38,49 @@ def minify_model(
     model: scvi.model.SCANVI,
     latent_qzm_key: str = "X_latent_qzm",
     latent_qzv_key: str = "X_latent_qzv",
+    save_dir: str = "lung_cancer_scanvi_minified"
 ) -> scvi.model.SCANVI:
+    """Minify the model and save it to disk."""
     qzm, qzv = model.get_latent_representation(return_dist=True)
     model.adata.obsm[latent_qzm_key] = qzm
     model.adata.obsm[latent_qzv_key] = qzv
     model.minify_adata(use_latent_qzm_key=latent_qzm_key, use_latent_qzv_key=latent_qzv_key)
 
+    model_out = os.path.join("models", save_dir)
+    make_parents(model_out)
+    model.save(model_out)
+
     return model
 
 
-def create_hub_model(model: scvi.model.SCANVI):
-    pass
+def create_hub_model(model_dir: str):
+    model_in = os.path.join("models", model_dir)
+
+    metadata = scvi.hub.HubMetadata.from_dir(
+        model_in,
+        anndata_version=ad.__version__,
+        training_data_url="https://zenodo.org/record/7227571/files/core_atlas_scanvi_model.tar.gz",
+    )
+
+    citation = r""""""
+    description = r""""""
+    card = scvi.hub.HubModelCardHelper.from_dir(
+        model_in,
+        license_info="cc-by-4.0",
+        anndata_version=ad.__version__,
+        data_is_minified=True,
+        data_is_annotated=True,
+        tissues=[],
+        training_data_url="https://zenodo.org/record/7227571/files/core_atlas_scanvi_model.tar.gz",
+        training_code_url="https://github.com/icbi-lab/luca",
+        description=description,
+        references=citation,
+        data_modalities=["rna"],
+    )
+
+    hubmodel = scvi.hub.HubModel(model_in, metadata=metadata, model_card=card)
+
+    return hubmodel
 
 
 def upload_hub_model(
@@ -48,7 +88,9 @@ def upload_hub_model(
     repo_name: str = "non_small_cell_lung_cancer",
     repo_token: str = None,
 ):
-    pass
+    hubmodel.push_to_huggingface_hub(
+        repo_name=repo_name, repo_token=repo_token, repo_create=True,
+    )
 
 
 if __name__ == "__main__":
