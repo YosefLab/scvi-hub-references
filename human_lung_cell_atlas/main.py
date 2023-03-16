172f4558ffe36c6318666ff6c4fdc90cfae63c0d
Martin Kim <46072231+martinkim0@users.noreply.github.com>
2023-03-16 16:25:24 -0700

    Update hlca and nsclc models (#17)

--- human_lung_cell_atlas/main.py
+++ human_lung_cell_atlas/main.py
@@ -1,122 +1,174 @@
-"""Upload HLCA."""
-import gc
+"""Human Lung Cell Atlas."""
+import json
+import os
+import pathlib
+import sys
 import tempfile
 from pathlib import Path
 
-import anndata
+import anndata as ad
 import pooch
+import scanpy as sc
 import scvi
-import snakemake
-from scvi.data._download import _download
 from scvi.hub import HubMetadata, HubModel, HubModelCardHelper
-from scvi.model.base import ArchesMixin
 
-scvi.settings.seed = 0
+HF_API_TOKEN = os.environ["HF_API_TOKEN"]
+scvi.settings.seed = 2023
 scvi.settings.reset_logging_handler()
 sys.stderr = open(snakemake.log[0], "w")  # noqa: F821
 sys.stdout = open(snakemake.log[1], "w")  # noqa: F821
-HF_TOKEN = snakemake.params["hf_api_token"]
 
-tmpdir = tempfile.TemporaryDirectory()
 
-# download the model
-model_url = "https://zenodo.org/record/6337966/files/HLCA_reference_model.zip"
-unzipped = pooch.retrieve(
-    url=model_url,
-    fname=f"HLCA_reference_model",
-    known_hash=None,
-    processor=pooch.Unzip(),
-    path=tmpdir.name,
-)
+def make_parents(*paths) -> None:
+    """Make parent directories of a file path if they do not exist."""
+    for p in paths:
+        pathlib.Path(p).parent.mkdir(parents=True, exist_ok=True)
 
-# convert legacy save format to new format:
-legacy_model_dir = Path(unzipped[0]).parent
-new_model_dir = legacy_model_dir.parent / "HLCA_reference_model_new"
-scvi.model.SCANVI.convert_legacy_save(legacy_model_dir, new_model_dir)
 
-# download the dataset
-adata = scvi.data.cellxgene(
-    "https://cellxgene.cziscience.com/e/066943a2-fdac-4b29-b348-40cede398e4e.cxg/",
-    filename="adata.h5ad",
-    save_path=new_model_dir.parent,
-)
+def load_config(config_path: str) -> dict:
+    """Load a JSON configuration file as a Python dictionary."""
+    with open(config_path) as f:
+        config = json.load(f)
+    return config
 
-# do some minimal processing of the data so it's ready for use with the model
-# get the raw, unnormalized counts
-adata.X = adata.raw.X
-del adata.raw
-# filter to highly variable genes
-adata = adata[:, adata.var.highly_variable == True].copy()
-# 4 genes are missing from cellxgene that are in the model. prepare_query_anndata will pad them with zeros.
-ArchesMixin.prepare_query_anndata(adata, new_model_dir)
 
-# load the model
-model = scvi.model.SCANVI.load(new_model_dir, adata=adata)
+def load_adata(savedir: str, config: dict) -> ad.AnnData:
+    """Load the dataset."""
+    adata = scvi.data.cellxgene(
+        config["adata_url"], filename=config["adata_fname"], save_path=savedir
+    )
+    return adata
 
-# get the latent representation
-_, qzv = model.get_latent_representation(give_mean=False, return_dist=True)
-model.adata.obsm["X_latent_qzv"] = qzv
-# for qzm, we'll use the qzm pre-computed by the HLCA team. We can get this from Zenodo.
-emb_url = "https://zenodo.org/record/6337966/files/HLCA_emb_and_metadata.h5ad"
-_download(emb_url, new_model_dir.parent, save_fn="adata_emb.h5ad")
-bdata = anndata.read_h5ad(Path(new_model_dir).parent / "adata_emb.h5ad")
-model.adata.obsm["X_latent_qzm"] = bdata.X
-del bdata
-gc.collect()
 
-# minify the data
-model.minify_adata()
+def preprocess_adata(adata: ad.AnnData, model_dir: str) -> ad.AnnData:
+    """Minimal preprocessing for the model."""
+    adata.X = adata.raw.X
+    adata = adata[:, adata.var.highly_variable == True].copy()  # noqa: E712
+    scvi.model.base.ArchesMixin.prepare_query_anndata(adata, model_dir)
 
-# save the model and minified adata
-# before we can do this we need to massage the adata so it's type are amenable to saving (str)
-model.adata.var.feature_name = model.adata.var.feature_name.cat.add_categories(
-    "Unknown"
-)
-model.adata.var.feature_reference = (
-    model.adata.var.feature_reference.cat.add_categories("Unknown")
-)
-model.adata.var.feature_biotype = model.adata.var.feature_biotype.cat.add_categories(
-    "Unknown"
-)
-model.adata.var.fillna("Unknown", inplace=True)
-obj_cols = model.adata.var.select_dtypes(include="object").columns
-model.adata.var.loc[:, obj_cols] = model.adata.var.loc[:, obj_cols].astype("str")
-# now save
-model_dir = (new_model_dir.parent / "HLCA_reference_model_new_minified",)
-model.save(
-    model_dir,
-    save_anndata=True,
-)
+    return adata
 
-# upload the model and minified data to hugging face
-# create metadata
-hm = HubMetadata.from_dir(
-    model_dir,
-    anndata_version=anndata.__version__,
-    training_data_url="https://cellxgene.cziscience.com/e/066943a2-fdac-4b29-b348-40cede398e4e.cxg/",
-)
-# create model card
-citation = r"""An integrated cell atlas of the human lung in health and disease
-L Sikkema, D Strobl, L Zappia, E Madissoon, NS Markov, L Zaragosi, M Ansari, M Arguel, L Apperloo, C Bécavin, M Berg, E Chichelnitskiy, M Chung, A Collin, ACA Gay, B Hooshiar Kashani, M Jain, T Kapellos, TM Kole, C Mayr, M von Papen, L Peter, C Ramírez-Suástegui, J Schniering, C Taylor, T Walzthoeni, C Xu, LT Bui, C de Donno, L Dony, M Guo, AJ Gutierrez, L Heumos, N Huang, I Ibarra, N Jackson, P Kadur Lakshminarasimha Murthy, M Lotfollahi, T Tabib, C Talavera-Lopez, K Travaglini, A Wilbrey-Clark, KB Worlock, M Yoshida, Lung Biological Network Consortium, T Desai, O Eickelberg, C Falk, N Kaminski, M Krasnow, R Lafyatis, M Nikolíc, J Powell, J Rajagopal, O Rozenblatt-Rosen, MA Seibold, D Sheppard, D Shepherd, SA Teichmann, A Tsankov, J Whitsett, Y Xu, NE Banovich, P Barbry, TE Duong, KB Meyer, JA Kropski, D Pe’er, HB Schiller, PR Tata, JL Schultze, AV Misharin, MC Nawijn, MD Luecken, F Theis
-bioRxiv 2022.03.10.483747; doi: https://doi.org/10.1101/2022.03.10.483747"""
-desc = r"""The first integrated, universal transcriptomic reference of the human lung on the single-cell level.
-For more details, see https://github.com/LungCellAtlas/HLCA."""
-hmch = HubModelCardHelper.from_dir(
-    model_dir,
-    license_info="cc-by-4.0",
-    anndata_version=anndata.__version__,
-    data_is_minified=True,
-    data_is_annotated=True,
-    tissues=["nose", "respiratory airway", "lung parenchyma"],
-    training_data_url="https://cellxgene.cziscience.com/e/066943a2-fdac-4b29-b348-40cede398e4e.cxg/",
-    training_code_url="https://github.com/LungCellAtlas/HLCA_reproducibility",
-    description=desc,
-    references=citation,
-    data_modalities=["rna"],
-)
-# create model
-hmo = HubModel(model_dir, metadata=hm, model_card=hmch)
-# push
-hmo.push_to_huggingface_hub(
-    repo_name="scvi-tools/human-lung-cell-atlas", repo_token=HF_TOKEN, repo_create=True
-)
+
+def postprocess_adata(adata: ad.AnnData) -> ad.AnnData:
+    """Postprocessing so the AnnData types are amenable to saving."""
+    for k in ["feature_name", "feature_reference", "feature_biotype"]:
+        new = getattr(adata.var, k).cat.add_categories("Unknown")
+        setattr(adata.var, k, new)
+
+    adata.var.fillna("Unknown", inplace=True)
+    obj_cols = adata.var.select_dtypes(include="object").columns
+    adata.var.loc[:, obj_cols] = adata.var.loc[:, obj_cols].astype("str")
+
+    return adata
+
+
+def convert_legacy_model(savedir: str, config: dict) -> str:
+    """Convert the legacy model to the new format."""
+    unzipped = pooch.retrieve(
+        url=config["legacy_model_url"],
+        fname=config["legacy_model_dir"],
+        known_hash=config["known_hash"],
+        processor=pooch.Unzip(),
+        path=savedir,
+    )
+    legacy_model_dir = Path(unzipped[0]).parent
+    new_model_dir = os.path.join(savedir, config["new_model_dir"])
+    make_parents(new_model_dir)
+    scvi.model.SCANVI.convert_legacy_save(legacy_model_dir, new_model_dir)
+
+    return new_model_dir
+
+
+def load_model(model_dir: str, adata: ad.AnnData) -> scvi.model.SCANVI:
+    """Load the model."""
+    return scvi.model.SCANVI.load(model_dir, adata=adata)
+
+
+def minify_model_and_save(
+    model: scvi.model.SCANVI,
+    savedir: str,
+    config: dict,
+    latent_qzm_key: str = "X_scanvi_qzm",
+    latent_qzv_key: str = "X_scanvi_qzv",
+) -> str:
+    """Minify the model and save it."""
+    _, qzv = model.get_latent_representation(give_mean=False, return_dist=True)
+
+    # use pre-computed qzm from HLCA team
+    emb_path = os.path.join(savedir, config["emb_fname"])
+    adata = sc.read(emb_path, backup_url=config["emb_url"])
+    qzm = adata.X
+
+    model.adata.obsm[latent_qzm_key] = qzm
+    model.adata.obsm[latent_qzv_key] = qzv
+    model.minify_adata(
+        use_latent_qzm_key=latent_qzm_key, use_latent_qzv_key=latent_qzv_key
+    )
+
+    model_dir = os.path.join(savedir, config["minified_model_dir"])
+    make_parents(model_dir)
+    model.save(model_dir, overwrite=True, save_anndata=True)
+
+    return model_dir
+
+
+def create_hub_model(model_dir: str, config: str) -> HubModel:
+    """Create a HubModel object."""
+    metadata = HubMetadata.from_dir(
+        model_dir,
+        anndata_version=ad.__version__,
+        training_data_url=config["adata_url"],
+    )
+
+    card = HubModelCardHelper.from_dir(
+        model_dir,
+        license_info=config["license_info"],
+        anndata_version=ad.__version__,
+        data_is_minified=True,
+        data_is_annotated=True,
+        tissues=config["tissues"],
+        training_data_url=config["adata_url"],
+        training_code_url=config["training_code_url"],
+        description=config["description"],
+        references=config["citation"],
+        data_modalities=config["data_modalities"],
+    )
+
+    return HubModel(model_dir, metadata=metadata, model_card=card)
+
+
+def upload_hub_model(hub_model: HubModel, repo_token: str, config: dict):
+    """Upload the model to the HuggingFace Hub."""
+    try:
+        hub_model.push_to_huggingface_hub(
+            repo_name=config["repo_name"],
+            repo_token=repo_token,
+            repo_create=True,
+        )
+    except Exception:
+        hub_model.push_to_huggingface_hub(
+            repo_name=config["repo_name"],
+            repo_token=repo_token,
+            repo_create=False,
+        )
+
+
+def main():
+    """Run main."""
+    config = load_config(snakemake.input[0])  # noqa: F821
+    savedir = tempfile.TemporaryDirectory().name
+
+    model_dir = convert_legacy_model(savedir, config)
+    adata = load_adata(savedir, config)
+    adata = preprocess_adata(adata, model_dir, config)
+    model = load_model(model_dir, adata)
+
+    model.adata = postprocess_adata(model.adata)
+    model_dir = minify_model_and_save(model, savedir, config)
+
+    hub_model = create_hub_model(model_dir, config)
+    upload_hub_model(hub_model, HF_API_TOKEN, config)
+
+
+if __name__ == "__main__":
+    main()
