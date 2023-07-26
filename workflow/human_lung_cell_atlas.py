6bc6203a1f47f40de8744d998f7fb43e5e63cc1a
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2023-07-26 08:56:00 -0700

    Update HLCA (#23)

--- workflow/human_lung_cell_atlas.py
+++ workflow/human_lung_cell_atlas.py
@@ -1,23 +1,12 @@
 """Human Lung Cell Atlas."""
-import os
 import sys
 
 import anndata as ad
-import numpy as np
 import pooch
-import scanpy as sc
 import scvi
-
-from utils import (
-    HF_API_TOKEN,
-    create_hub_model,
-    get_temp_dir,
-    load_adata,
-    load_config, 
-    minify_model_and_save,
-    retrieve_and_convert_legacy_model,
-    upload_hub_model,
-)
+from utils import (HF_API_TOKEN, create_hub_model, get_temp_dir, load_adata,
+                   load_config, minify_model_and_save,
+                   retrieve_and_convert_legacy_model, upload_hub_model)
 
 sys.stderr = open(snakemake.log[0], "w")  # noqa: F821
 sys.stdout = open(snakemake.log[1], "w")  # noqa: F821
@@ -26,7 +15,17 @@ sys.stdout = open(snakemake.log[1], "w")  # noqa: F821
 def preprocess_adata(adata: ad.AnnData, model_dir: str) -> ad.AnnData:
     """Minimal preprocessing for the model."""
     adata.X = adata.raw.X
-    adata = adata[:, adata.var.highly_variable == True].copy()  # noqa: E712
+    _, mvars, _, _ = scvi.model.base._utils._load_saved_files(
+        model_dir, load_adata=False
+    )
+    adata = adata[:, adata.var.index.isin(mvars)].copy()
+
+    # get rid of some var columns that we dont need
+    # -- will make later processing easier
+    del adata.var["feature_is_filtered"]
+    del adata.var["feature_reference"]
+    del adata.var["feature_biotype"]
+
     scvi.model.base.ArchesMixin.prepare_query_anndata(adata, model_dir)
 
     return adata
@@ -34,13 +33,18 @@ def preprocess_adata(adata: ad.AnnData, model_dir: str) -> ad.AnnData:
 
 def postprocess_adata(adata: ad.AnnData) -> ad.AnnData:
     """Postprocessing so the AnnData types are amenable to saving."""
-    for k in ["feature_name", "feature_reference", "feature_biotype"]:
-        new = getattr(adata.var, k).cat.add_categories("Unknown")
-        setattr(adata.var, k, new)
-
-    adata.var.fillna("Unknown", inplace=True)
-    obj_cols = adata.var.select_dtypes(include="object").columns
-    adata.var.loc[:, obj_cols] = adata.var.loc[:, obj_cols].astype("str")
+    # add feature_names for the padded genes
+    gene_ids = [
+        "ENSG00000253701",
+        "ENSG00000269936",
+        "ENSG00000274961",
+        "ENSG00000279576",
+    ]
+    feat_names = ["AL928768.3", "RP11-394O4.5", "RP3-492J12.2", "AP000769.1"]
+    c = adata.var["feature_name"].cat.add_categories(feat_names)
+    adata.var["feature_name"] = c
+    for g, f in zip(gene_ids, feat_names):
+        adata.var.loc[g, "feature_name"] = f
 
     return adata
 
@@ -60,22 +64,24 @@ def main():
         processor=pooch.Unzip(),
     )
     adata = load_adata(
-        url=config["adata_url"], 
-        fname=config["adata_fname"], 
-        save_dir=save_dir, 
+        url=config["adata_url"],
+        fname=config["adata_fname"],
+        save_dir=save_dir,
         cellxgene=True,
     )
     adata = preprocess_adata(adata, model_dir)
     model = scvi.model.SCANVI.load(model_dir, adata=adata)
 
     model.adata = postprocess_adata(model.adata)
-    qzm = load_adata(
+    adata_emb = load_adata(
         url=config["emb_url"], fname=config["emb_fname"], save_dir=save_dir
-    ).X
+    )
+    adata_emb = adata_emb[adata_emb.obs["core_or_extension"] == "core"].copy()
+    qzm = adata_emb[model.adata.obs.index].copy().X
     model_dir = minify_model_and_save(
         model=model,
         save_dir=save_dir,
-        model_dir= config["minified_model_dir"], 
+        model_dir=config["minified_model_dir"],
         qzm=qzm,
     )
 
